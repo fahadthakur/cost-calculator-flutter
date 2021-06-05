e3ff070214d7c8d85f58bd86359629ea91730173
Fahad Thakur <ft.thakur1@gmail.com>
2021-06-05 04:34:20 +0300

    Added the ability to save the review to a pdf file

--- lib/Screens/review.dart
+++ lib/Screens/review.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:costcalculator/cost.dart';
 import 'package:costcalculator/scrollingeffect.dart';
 import 'package:flutter/material.dart';
+
+import '../pdf_api.dart';
 
 class ReviewPage extends StatefulWidget {
   const ReviewPage({Key? key}) : super(key: key);
@@ -27,63 +31,84 @@ class _ReviewPageState extends State<ReviewPage> {
       ),
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
           child: Container(
-            child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'Review your chosen options',
-                    style:
-                        TextStyle(fontSize: 23.0, fontWeight: FontWeight.w600),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Text(
-                    'Industry: ${CostReview.industry}',
-                    style:
-                        TextStyle(fontSize: 23.0, fontWeight: FontWeight.w500),
-                  ),
-                  Expanded(
-                    child: ScrollConfiguration(
-                      behavior: MyBehavior(),
-                      child: ListView.builder(
-                        itemCount: CostReview.optionsChosen.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          String key =
-                              CostReview.optionsChosen.keys.elementAt(index);
-                          return new Column(
-                            children: <Widget>[
-                              new ListTile(
-                                title: new Text(
-                                  '$key',
-                                  style: TextStyle(
-                                      fontSize: 20.0,
-                                      fontWeight: FontWeight.w500),
+            child: Stack(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Review your chosen options',
+                      style: TextStyle(
+                          fontSize: 23.0, fontWeight: FontWeight.w600),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Text(
+                      'Industry: ${CostReview.industry}',
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.w500),
+                    ),
+                    Expanded(
+                      child: ScrollConfiguration(
+                        behavior: MyBehavior(),
+                        child: ListView.builder(
+                          itemCount: CostReview.optionsChosen.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            String key =
+                                CostReview.optionsChosen.keys.elementAt(index);
+                            return new Column(
+                              children: <Widget>[
+                                new ListTile(
+                                  title: new Text(
+                                    '$key',
+                                    style: TextStyle(
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                  subtitle: new Text(
+                                    '${CostReview.optionsChosen[key]}',
+                                    style: TextStyle(fontSize: 18.0),
+                                  ),
                                 ),
-                                subtitle: new Text(
-                                  '${CostReview.optionsChosen[key]}',
-                                  style: TextStyle(fontSize: 18.0),
+                                SizedBox(
+                                  height: 5.0,
                                 ),
-                              ),
-                              SizedBox(
-                                height: 5.0,
-                              ),
-                            ],
-                          );
-                        },
+                              ],
+                            );
+                          },
+                        ),
                       ),
                     ),
-                  ),
-                  Text('Estimated Cost: \$ ${CostReview.estimatedCost}',
+                    Text(
+                      'Estimated Cost: \$ ${CostReview.estimatedCost}',
                       style: TextStyle(
                         fontSize: 22.0,
                         fontWeight: FontWeight.w600,
-                      )),
-                ],
-              ),
+                      ),
+                    ),
+                  ],
+                ),
+                Positioned(
+                  bottom: 0.0,
+                  right: 0.0,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                    child: FloatingActionButton(
+                      child: Icon(
+                        Icons.save_rounded,
+                      ),
+                      onPressed: () async {
+                        print('Clicked');
+                        final pdfFile = await PdfApi.generateDocument();
+                        PdfApi.openFile(pdfFile);
+                      },
+                    ),
+                  ),
+                )
+              ],
             ),
           ),
         ),
---
+++ lib/pdf_api.dart
@@ -0,0 +1,86 @@
+import 'dart:io';
+
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart';
+
+import 'cost.dart';
+
+class ReviewQuestions {
+  String question;
+  String option;
+
+  ReviewQuestions(this.question, this.option);
+}
+
+class PdfApi {
+  static Future<File> generateDocument() async {
+    final pdf = Document();
+
+    pdf.addPage(
+      MultiPage(
+        pageFormat: PdfPageFormat.a4,
+        build: (context) => <Widget>[
+          buildCustomHeadline(),
+          SizedBox(height: 20.0),
+          buildTable(),
+          SizedBox(height: 20.0),
+          displayCost(),
+        ],
+      ),
+    );
+
+    return saveDocument(
+        name: 'Cost Review ${CostReview.industry}.pdf', pdf: pdf);
+  }
+
+  static Widget buildCustomHeadline() => Center(
+          child: Container(
+        padding: EdgeInsets.all(8.0),
+        decoration: BoxDecoration(border: Border.all(color: PdfColors.indigo)),
+        child: Text('Cost Review - ${CostReview.industry}',
+            style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold)),
+      ));
+
+  static Widget buildTable() {
+    final headers = ['Question', 'Option Chosen'];
+
+    final questions = [];
+    CostReview.optionsChosen
+        .forEach((k, v) => questions.add(ReviewQuestions(k, v)));
+    final data = questions.map((e) => [e.question, e.option]).toList();
+    return Table.fromTextArray(
+        headers: headers,
+        data: data,
+        headerStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+        cellStyle: TextStyle(fontSize: 14.0));
+  }
+
+  static Widget displayCost() {
+    final cost = CostReview.estimatedCost;
+
+    return new Text('Total estimated cost: \$$cost',
+        style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold));
+  }
+
+  static Future<File> saveDocument({
+    required String name,
+    required Document pdf,
+  }) async {
+    final bytes = await pdf.save();
+
+    final dir = await getApplicationDocumentsDirectory();
+    final file = File('${dir.path}/$name');
+
+    await file.writeAsBytes(bytes);
+
+    return file;
+  }
+
+  static Future openFile(File file) async {
+    final url = file.path;
+
+    await OpenFile.open(url);
+  }
+}
