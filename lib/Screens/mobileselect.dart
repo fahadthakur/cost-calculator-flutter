33ce7830ea04bbefe42facbaffeb42a08f2c05cd
Fahad Thakur <ft.thakur1@gmail.com>
2021-06-05 13:21:22 +0300

    Added a willpop method to the pages

--- lib/Screens/mobileselect.dart
+++ lib/Screens/mobileselect.dart
@@ -33,89 +33,92 @@ class _MobileSelectState extends State<MobileSelect> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-            onPressed: () {
-              CostReview.optionsChosen.clear();
-              CostReview.estimatedCost = 0;
-              print(CostReview.optionsChosen);
-              print(CostReview.estimatedCost);
-              Navigator.pop(context);
-            },
-            icon: Icon(Icons.arrow_back_ios_new_rounded)),
-        title: Text(
-          "Mobile App Cost Calculator",
-          style: TextStyle(fontSize: 22.0),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+              onPressed: () {
+                CostReview.optionsChosen.clear();
+                CostReview.estimatedCost = 0;
+                print(CostReview.optionsChosen);
+                print(CostReview.estimatedCost);
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.arrow_back_ios_new_rounded)),
+          title: Text(
+            "Mobile App Cost Calculator",
+            style: TextStyle(fontSize: 22.0),
+          ),
         ),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 80.0),
-                child: Container(
-                  child: ScrollConfiguration(
-                    behavior: MyBehavior(),
-                    child: ListView.builder(
-                        itemBuilder: (_, int index) => QuestionWidget(
-                            questions[index].question,
-                            questions[index].options,
-                            questions[index].cost),
-                        itemCount: questions.length),
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 80.0),
+                  child: Container(
+                    child: ScrollConfiguration(
+                      behavior: MyBehavior(),
+                      child: ListView.builder(
+                          itemBuilder: (_, int index) => QuestionWidget(
+                              questions[index].question,
+                              questions[index].options,
+                              questions[index].cost),
+                          itemCount: questions.length),
+                    ),
                   ),
                 ),
-              ),
-              Positioned(
-                bottom: 0.0,
-                child: Column(
-                  children: [
-                    Text(
-                      'Current Estimated Cost: \$' +
-                          CostReview.estimatedCost.toString(),
-                      style: TextStyle(
-                          fontSize: 20.0, fontWeight: FontWeight.w500),
-                    ),
-                    Row(
-                      //crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        ElevatedButton.icon(
-                          onPressed: () {
-                            print('Clciked');
-                            setState(() {});
-                          },
-                          icon: Icon(Icons.update_rounded),
-                          label: Text(
-                            'Update Cost',
-                            style: TextStyle(fontSize: 20.0),
+                Positioned(
+                  bottom: 0.0,
+                  child: Column(
+                    children: [
+                      Text(
+                        'Current Estimated Cost: \$' +
+                            CostReview.estimatedCost.toString(),
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.w500),
+                      ),
+                      Row(
+                        //crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          ElevatedButton.icon(
+                            onPressed: () {
+                              print('Clciked');
+                              setState(() {});
+                            },
+                            icon: Icon(Icons.update_rounded),
+                            label: Text(
+                              'Update Cost',
+                              style: TextStyle(fontSize: 20.0),
+                            ),
                           ),
-                        ),
-                        SizedBox(
-                          width: 10.0,
-                        ),
-                        ElevatedButton.icon(
-                          onPressed: () {
-                            print(CostReview.optionsChosen);
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => ReviewPage()));
-                          },
-                          icon: Icon(Icons.reviews_rounded),
-                          label: Text(
-                            'Review Chosen Options',
-                            style: TextStyle(fontSize: 20.0),
+                          SizedBox(
+                            width: 10.0,
                           ),
-                        )
-                      ],
-                    )
-                  ],
+                          ElevatedButton.icon(
+                            onPressed: () {
+                              print(CostReview.optionsChosen);
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => ReviewPage()));
+                            },
+                            icon: Icon(Icons.reviews_rounded),
+                            label: Text(
+                              'Review Chosen Options',
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
